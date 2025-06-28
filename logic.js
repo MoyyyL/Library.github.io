9ccfd2ad234c4434b0e4dee6946bf070f0485399
MoyyyL <moyyyl0526@gmail.com>
2025-06-28 17:12:50 -0600

    getting data from the form and adding the new book to the array

---
+++ logic.js
@@ -0,0 +1,44 @@
+//* FUNCTIONS -----------------------------------------------
+let library = [];
+
+// constructor de los objetos libro
+function Book(id, title, author, pages, summary, read) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.summary = summary;
+    this.read = read;
+}
+
+// Genera el id y asigna todos los valores al nuevo objeto libro
+// Despues agrega el nuevo objeto libro al array
+function addToLibrary(title = "", author = "", pages = 0, summary = "") {
+    let id = crypto.randomUUID();
+    let read = false;
+
+    const nthBook = new Book(id, title, author, pages, summary, read)
+    library.push(nthBook);
+}
+
+//todo FUNCION QUE VA A DESPLEGAR EL LIBRO EN LA UI
+
+//* GETTING DATA -----------------------------------------------
+const Title = document.querySelector("#title");
+const Author = document.querySelector("#author");
+const Pages = document.querySelector("#pages");
+const Summary = document.querySelector("#summary");
+
+//* SIGNAL -----------------------------------------------
+const submit = document.querySelector("#send")
+submit.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    let valTitle = Title.value;
+    let valAuthor = Author.value;
+    let valPages = Pages.value;
+    let valSummary = Summary.value;
+
+    addToLibrary(valTitle, valAuthor, valPages, valSummary);
+    console.log(library);
+})
