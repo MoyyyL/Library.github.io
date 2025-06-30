ee918850fbc01e3e008b12fe996ba6b0619af8d6
MoyyyL <moyyyl0526@gmail.com>
2025-06-30 12:45:00 -0600

    better comments

--- logic.js
+++ logic.js
@@ -25,25 +25,25 @@ submit.addEventListener("click", (event) => {
 //* DIALOG ------------------------------------------------------
 const dialog = document.querySelector(".library__book-info");
 
-// FUNCIONALIDADES AL PRIMER LIBRO
+// DEFAULT FUNCTIONS FOR THE DEFAULT BOOK
 const showButton = document.querySelector(".book button");
 showButton.addEventListener("click", () => {
     dialog_book_buttons(library[0]);
     dialog.showModal();
 })
 
-// BOTON DE CERRADO PARA EL DIALOG
+// CLOSE DIALOG
 const closeButton = document.querySelector(".library__book-info button");
 closeButton.addEventListener("click", () => {
     dialog.close();
 })
 
-// BOTON COMPARTIDO DE LEIDO
-const readButton = document.querySelector(".book-read"); // selecciona el boton en el DOM
+// READ / UNREAD BUTTON
+const readButton = document.querySelector(".book-read");
 readButton.addEventListener("click", () => {
-    const button_id = readButton.dataset.id; // Extrae el id del objeto
-    let asd = library.find((element) => element.id == button_id); // compara los id del array con el propio y retorna el objeto
-    if (asd.read === false) { // edita el contenido y valor del objeto
+    const button_id = readButton.dataset.id; // GETS THE CURRENT ID OF THE BUTTON
+    let asd = library.find((element) => element.id == button_id); // COMPARES THE ARRAYS IDS WITH OWN
+    if (asd.read === false) { // CHANGES THE VALUE AND TEXTCONTENT
         asd.read = true;
         readButton.textContent = "read";
     }
@@ -53,22 +53,22 @@ readButton.addEventListener("click", () => {
     }
 });
 
-// BOTON ELIMINAR
-const deleteButton = document.querySelector(".book-delete"); //selecciona delete buton en el DOM
+// DELETE BUTTON
+const deleteButton = document.querySelector(".book-delete");
 deleteButton.addEventListener("click", () => {
-   const delButton_id = deleteButton.dataset.id; // extrae su ID
-   const elpepe = document.querySelector(".library__books") // selecciona el elemento padre
-   const tilin = document.querySelector(`.book[data-id = "${delButton_id}"]`); // selecciona el elemento .book a eliminar
-   elpepe.removeChild(tilin); // lo remueve
+   const delButton_id = deleteButton.dataset.id; // GETS THE CURRENT ID
+   const elpepe = document.querySelector(".library__books") // SELECTS WHERE WE APPEND THE BOOKS (THE FATHER DIV)
+   const tilin = document.querySelector(`.book[data-id = "${delButton_id}"]`); // SELECTS THE CURRENT BOOK.ELEMENT (THE SON DIV)
+   elpepe.removeChild(tilin); // REMOVES
    
    let delIDX = library.findIndex((element) => element.id == delButton_id);
-   library.splice(delIDX, 1); // lo elimina del array
+   library.splice(delIDX, 1); // REMOVES FROM THE ARRAY
    dialog.close();
 });
 
 //* FUNCTIONS -----------------------------------------------
 
-// GENERA ID, CREA EL OBJETO, AGREGA AL ARRAY
+// GENERATES ID, CREATES THE BOOK OBJECT AND PUSHES IN THE ARRAY
 function addToLibrary(title = "", author = "", pages = 0, summary = "") {
     let id = crypto.randomUUID();
     let read = false;
@@ -77,7 +77,7 @@ function addToLibrary(title = "", author = "", pages = 0, summary = "") {
     library.push(nthBook);
 }
 
-// CONSTRUCTOR de los objetos libro
+// CONSTRUCTOR OF BOOKS
 function Book(id, title, author, pages, summary, read) {
     this.id = id;
     this.title = title;
@@ -87,17 +87,17 @@ function Book(id, title, author, pages, summary, read) {
     this.read = read;
 }
 
-// SELECCIONA EL LIBRO MAS RECIENTE
+// SELECTS THE LAST BOOK IN ARRAY AND CALL FUNCTION
 function array_books() {
     const last = library[library.length - 1];
     displayBook(last);
 }
 
-// SELECCIONA DOM, AGREGA EL LIBRO AL DOM, ASIGNA FUNCION A LOS BOTONES
-function displayBook(lastBook) { // lastbook is the array element
+// DISPLAY THE BOOK IN THE DOM
+function displayBook(lastBook) { // lastBook is the array element
     const name = lastBook.title;
-    const library_books = document.querySelector(".library__books"); //father
-    const book_div = document.createElement("div"); //son
+    const library_books = document.querySelector(".library__books");
+    const book_div = document.createElement("div");
     const book_button = document.createElement("button");
 
     book_button.textContent = name; //title of the book
@@ -108,13 +108,12 @@ function displayBook(lastBook) { // lastbook is the array element
     library_books.appendChild(book_div);
 
     book_button.addEventListener("click", () => {
-        dialog_book_buttons(lastBook);
+        dialog_book_buttons(lastBook); // asigning functionality to the buttons
     })
 }
 
 // EDITS THE DIALOG WITH THE INFO OF THE PRESSED BOOK
 function dialog_book_buttons(element) {
-    // selecciona el DOM
     const innerDialog = document.querySelector(".library__book-info");
 
     const book_title = document.querySelector(".book-title");
@@ -122,18 +121,17 @@ function dialog_book_buttons(element) {
     const book_pages = document.querySelector(".book-pages");
     const book_summary = document.querySelector(".book-summary");
 
-    //Edita el DOM
     book_title.textContent = element.title;
     book_author.textContent = element.author;
     book_pages.textContent = element.pages;
     book_summary.textContent = element.summary;
 
-    //Asigna id del elemento al boton y asigna valor inicial
+    //asigns id and initial value to the read-button
     const book_read = document.querySelector(".book-read");
     book_read.dataset.id = element.id;
     book_read.textContent = (element.read == false) ? "unread" : "read"; 
 
-    // Asigna el mismo id al .book
+    // asigns id to the delete-button
     const book_delete = document.querySelector(".book-delete");
     book_delete.dataset.id = element.id;
 
