696c0bd2e0b217c7fdae57e7b0e4c80b7716482e
MoyyyL <moyyyl0526@gmail.com>
2025-06-29 21:47:00 -0600

    Buggi-working version of the site

--- logic.js
+++ logic.js
@@ -20,10 +20,12 @@ function addToLibrary(title = "", author = "", pages = 0, summary = "") {
     library.push(nthBook);
 }
 
+//!--------------------------------------------------------------------------
 // SELECCIONA DOM, AGREGA EL LIBRO AL DOM, ASIGNA FUNCION A LOS BOTONES
-function displayBook(name) {
-    const library_books = document.querySelector(".library__books");
-    const book_div = document.createElement("div");
+function displayBook(lastBook) {
+    const name = lastBook.title;
+    const library_books = document.querySelector(".library__books"); //father
+    const book_div = document.createElement("div"); //son
     const book_button = document.createElement("button");
 
     book_div.classList.add("book");
@@ -32,21 +34,57 @@ function displayBook(name) {
     
     library_books.appendChild(book_div);
 
-//todo  FUNCION QUE ASIGNE EL SHOWMODA, ELIMINAR Y LEIDO/NO LEIDO EN EL DIALOG
     book_button.addEventListener("click", () => {
-        dialog.showModal();
+        //                  book-object, father, son
+        dialog_book_buttons(lastBook, library_books, book_div);
     })
 }
 
 // SELECCIONA EL LIBRO MAS RECIENTE
 function array_books() {
     const last = library[library.length - 1];
-    displayBook(last.title);
+    displayBook(last);
 }
 
-//! DIALOG ------------------------------------------------------
-// FUNCIONALIDADES AL PRIMER LIBRO
+// EDITS THE DIALOG WITH THE INFO OF THE PRESSED BOOK
+function dialog_book_buttons(element, father, son) {
+    const innerDialog = document.querySelector(".library__book-info");
+
+    const book_title = document.querySelector(".book-title");
+    const book_author = document.querySelector(".book-author");
+    const book_pages = document.querySelector(".book-pages");
+    const book_summary = document.querySelector(".book-summary");
+    const book_read = document.querySelector(".book-read");
+    const book_delete = document.querySelector(".book-delete");
+
+    book_title.textContent = element.title;
+    book_author.textContent = element.author;
+    book_pages.textContent = element.pages;
+    book_summary.textContent = element.summary;
+
+    book_read.addEventListener("click", () => {
+        if (element.read === false) {
+            element.read = true;
+            book_read.textContent = "Read";
+        }
+        else if (element.read === true) {
+            element.read = false;
+            book_read.textContent = "Not read"
+        }
+    })
+
+    book_delete.addEventListener("click", () => {
+        innerDialog.close();
+        father.removeChild(son);
+    })
+
+    innerDialog.showModal();
+}
+
+//* DIALOG ------------------------------------------------------
 const dialog = document.querySelector(".library__book-info");
+
+// FUNCIONALIDADES AL PRIMER LIBRO
 const showButton = document.querySelector(".book button");
 showButton.addEventListener("click", () => {
     dialog.showModal();
@@ -58,16 +96,14 @@ closeButton.addEventListener("click", () => {
     dialog.close();
 })
 
-//! CHAMBA ------------------------------------------------------
-
-
-//* GETTING DATA -----------------------------------------------
+//* FORM --------------------------------------------------
+// GETTING DATA
 const Title = document.querySelector("#title");
 const Author = document.querySelector("#author");
 const Pages = document.querySelector("#pages");
 const Summary = document.querySelector("#summary");
 
-//* SIGNAL -----------------------------------------------
+// SIGNAL
 const submit = document.querySelector("#send")
 submit.addEventListener("click", (event) => {
     event.preventDefault();
