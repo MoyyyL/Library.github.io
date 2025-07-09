16aa7fbf461dd62aeae73bbb71864e8a9a5b1af9
MoyyyL <moyyyl0526@gmail.com>
2025-07-09 17:58:49 -0600

    already working proyect

--- logic2.js
+++ logic2.js
@@ -66,24 +66,114 @@ class manageLibrary {
     }
 }
 
-//const lib = new manageLibrary();
-//lib.addBook("the hobbit", "me", 198, "okay");
-
-
 //* ---------------DOM management ------------------------
 class DOMController {
     Library = new manageLibrary();
     
-    formHandler() {
-        const submit = document.querySelector("#send");
-        submit.addEventListener("click", (e) => {
-            e.preventDefault();
-            const title = document.querySelector("#title");
-            const author = document.querySelector("#author");
-            const pages = document.querySelector("#pages");
-            const summary = document.querySelector("#summary");
+    formHandler(event) {
+        event.preventDefault();
+        console.log("handled")
 
-            
+        const title = document.querySelector("#title")
+        const author = document.querySelector("#author")
+        const pages = document.querySelector("#pages")
+        const summary = document.querySelector("#summary")
+
+        const valTitle = title.value;
+        const valAuthor = author.value;
+        const valPages = pages.value;
+        const valSummary = summary.value;
+        
+        this.Library.addBook(valTitle, valAuthor, valPages, valSummary)
+        this.renderBook()
+    }
+
+    renderBook() {
+        const actuallLibrary = this.Library.getLibrary;
+        const table = document.querySelector(".library__books");
+        const domBook = document.createElement("div");
+        const button = document.createElement("button");
+        
+        domBook.classList.add("book");
+        domBook.dataset.id = actuallLibrary[actuallLibrary.length - 1].id;        
+        button.textContent = actuallLibrary[actuallLibrary.length - 1].title;
+        
+        domBook.appendChild(button);
+        table.appendChild(domBook);
+    }
+
+    eraseBook(id) {
+        this.Library.delBook(id);
+        const table = document.querySelector(".library__books");
+        const domDelBook = document.querySelector(`.book[data-id="${id}"]`)
+        table.removeChild(domDelBook);
+    }
+
+    readBook(val, id) {
+        this.Library.updateBook(val, id);
+    }
+
+    manageDialog(id) {
+        const actuallLibrary = this.Library.getLibrary;
+        const dialog = document.querySelector(".library__book-info");
+
+        const specificBook = actuallLibrary.find(element => element.id === id);
+        
+        console.log(specificBook)
+        const read = (specificBook.read === false) ? "Not Read" : "Read";
+        
+        dialog.innerHTML = `
+                <button>X</button>
+                
+                <h3>Title:</h3>
+                <p class="book-title">${specificBook.title}</p>
+                
+                <h3>Author:</h3>
+                <p class="book-author">${specificBook.author}</p>
+                
+                <h3>Pages</h3>
+                <p class="book-pages">${specificBook.pages}</p>
+                
+                <h3>Summary</h3>
+                <p class="book-summary">${specificBook.summary}</p>
+                
+                <button class="book-read" data-id="${specificBook.id}">${read}</button>
+                <button class="book-delete" data-id="${specificBook.id}">Delete</button>
+            `;
+        
+        const delButton = document.querySelector(".book-delete");
+        delButton.addEventListener("click", () => {
+            this.eraseBook(delButton.dataset.id);
+            dialog.close();
         })
+
+        const readButton = document.querySelector(".book-read");
+        readButton.addEventListener("click", () => {
+            const value = !specificBook.read;
+            this.readBook(value ,readButton.dataset.id)
+            readButton.textContent = (specificBook.read === false) ? "Not read" : "Read";
+            console.log(specificBook)
+        })
+
+        const closeButton = document.querySelector("button")
+        closeButton.addEventListener("click", () => dialog.close())
+        dialog.showModal();
     }
 }
+const control = new DOMController();
+
+const submit = document.querySelector("#send");
+submit.addEventListener("click", (e) => {
+    control.formHandler(e);
+});
+
+const table = document.querySelector(".library__books");
+table.addEventListener("click", (e) => {
+    const bookElement = e.target.closest(".book");
+    
+    console.log(bookElement.dataset.id)
+
+    if (bookElement) {
+        control.manageDialog(bookElement.dataset.id);
+    }
+})
